7fe9bfee7e196148bf7e9a439867a6b8b20fa20b
Melissa F <melissa.faulner@gmail.com>
2020-11-30 17:18:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |dwarf, index|
-    puts "#{index}. #{dwarf}"
+    puts "#{index}. #{dwarf}.Capitalize"
   end
 end
